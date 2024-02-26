8779cfd339612435b223b9801793318e1a71ccaa
Charly-programmeur <charlynoudjalta.com>
2024-02-26 00:44:58 +0000

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+//import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import product from './product';
+import Name from './components/Name';
+import Price from './components/Price';
+import Description from './components/Description';
+import Image from './components/Image';
+import { Card } from 'react-bootstrap';
 
-function App() {
+
+const firstName = "Charly Djitaïngué Noudjalta";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '18rem' }}>
+        <Image imageUrl={product.image} />
+        <Card.Body>
+          <Name name={product.name} />
+          <Price price={product.price} />
+          <Description description={product.description} />
+        </Card.Body>
+      </Card>
+      <p>Hello, {firstName ? firstName : "there"}!</p>
+      {firstName && <img src="https://static.vecteezy.com/ti/vecteur-libre/p1/4566360-caissier-homme-personnage-illustration-vectoriel.jpg" alt="Your First Name" />}
     </div>
   );
 }
---
+++ src/product.js
@@ -0,0 +1,8 @@
+const product = {
+  name: "Baskets blanches",
+  price: "$19.99",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  image: "https://blog.therunningcollective.fr/wp-content/uploads/2023/12/Peg-41-1024x586.jpeg"
+};
+
+export default product;
